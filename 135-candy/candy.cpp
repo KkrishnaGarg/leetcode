48017a20dd4af4d5a87d4416b9ab88792ebb9928
KkrishnaGarg <24ucc029@lnmiit.ac.in>
2026-07-08 01:50:38 +0530

    Time: 0 ms (100.00%) | Memory: 22.9 MB (52.30%) - LeetSync

---
+++ 135-candy/candy.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        vector<int> distribution(n, 1);
+
+        // Left to right
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                distribution[i] = distribution[i - 1] + 1;
+            }
+        }
+
+        // Right to left
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1] && distribution[i] <= distribution[i+1]) {
+                distribution[i] = distribution[i + 1] + 1;
+            }
+        }
+
+        int total = 0;
+        for (int candy : distribution) {
+            total += candy;
+        }
+
+        return total;
+    }
+};
