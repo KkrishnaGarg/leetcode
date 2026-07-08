72703f97dcc756ad812a0005f3c1825b4278d06b
KkrishnaGarg <24ucc029@lnmiit.ac.in>
2026-07-08 20:39:08 +0530

    Time: 3 ms (40.41%) | Memory: 33 MB (55.78%) - LeetSync

--- 1372-check-if-it-is-a-good-array/check-if-it-is-a-good-array.cpp
+++ 1372-check-if-it-is-a-good-array/check-if-it-is-a-good-array.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     bool isGoodArray(vector<int>& nums) {
-        int x = nums[0];
-        for(int i=1; i<nums.size(); i++){
+        int x = nums[0], n=nums.size();
+        for(int i=1; i<n; i++){
             x = gcd(x, nums[i]);
             if(x==1) return true;
         }
