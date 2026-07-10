692af5ea5dcec8b9fa600c8a596fdfe25f2d99b7
KkrishnaGarg <24ucc029@lnmiit.ac.in>
2026-07-10 17:17:39 +0530

    Time: 3 ms (44.87%) | Memory: 10.9 MB (8.00%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0) return false;
+        string s = to_string(x);
+        int n = s.size(), i=0;
+        while(i<n/2){
+            if(s[i]!=s[n-1-i]) return false;
+            i++;
+        }
+        return true;
+    }
+};
