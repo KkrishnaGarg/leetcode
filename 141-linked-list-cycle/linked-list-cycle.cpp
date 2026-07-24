16f96f6e204dab1ffb84ef5edd19cefb8e8cfccd
KkrishnaGarg <24ucc029@lnmiit.ac.in>
2026-07-25 04:42:10 +0530

    Time: 9 ms (55.04%) | Memory: 11.9 MB (54.55%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -9,17 +9,11 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        if(head==nullptr || head->next==nullptr) return false;
-        unordered_map<ListNode*, int> visited;
-
-        ListNode* temp = head;
-
-        while (temp) {
-            if (visited[temp]) {
-                return true;
-            }
-            visited[temp] = 1;
-            temp = temp->next;
+        ListNode *slow = head, *fast = head;
+        while(fast && fast->next){
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow == fast) return true;
         }
         return false;
     }
