b80be0df837464b3ab47832d08d8544c29793722
KkrishnaGarg <24ucc029@lnmiit.ac.in>
2026-07-29 05:48:02 +0530

    Time: 79 ms (41.43%) | Memory: 53.9 MB (98.48%) - LeetSync

--- 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.cpp
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.cpp
@@ -3,17 +3,9 @@ public:
     string smallestPalindrome(string s) {
         int n = s.size();
         int m = n/2;
-        if(n&1){
-            sort(s.begin(), s.begin()+m);
-            for(int i=m; i<n; i++){
-                s[i] = s[n-i-1];
-            }
-        }
-        else{
-            sort(s.begin(), s.begin()+m);
-            for(int i=m; i<n; i++){
-                s[i] = s[n-i-1];
-            }
+        sort(s.begin(), s.begin()+m);
+        for(int i=m; i<n; i++){
+            s[i] = s[n-i-1];
         }
         return s;
     }
