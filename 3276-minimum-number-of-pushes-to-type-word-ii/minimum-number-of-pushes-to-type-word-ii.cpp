e476e014e9b41c37081834af65b3169d10ffa581
KkrishnaGarg <24ucc029@lnmiit.ac.in>
2026-07-30 06:43:31 +0530

    Time: 2 ms (94.55%) | Memory: 25.4 MB (84.36%) - LeetSync

---
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        vector<int> hash(26, 0);
+        for(auto c : word){
+            hash[c-'a']++;
+        }
+        sort(hash.begin(), hash.end(), greater<int>());
+        int n=0;
+        for(int i=0; i<26; i++){
+            if(hash[i]>0) n++;
+            else break;
+        }
+
+        int sum = 0;
+        for(int i=0; i<n; i++){
+            if(i<8) sum += hash[i];
+            else if(i<16) sum += hash[i]*2;
+            else if(i<24) sum += hash[i]*3;
+            else sum += hash[i]*4;
+        }
+        return sum;
+    }
+};
