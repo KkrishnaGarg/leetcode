71e927393e27d2f15ded8844a50bb492e061b7d8
KkrishnaGarg <24ucc029@lnmiit.ac.in>
2026-07-30 05:53:25 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (11.73%) - LeetSync

---
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        unordered_set<char> st;
+        for (auto c : word)
+            st.insert(c);
+
+        int n = st.size();
+        if(n<=8) return n;
+        else if(n<=16) return 8 + (n-8)*2;
+        else if(n<=24) return 24 + (n-16)*3;
+        return 48 + (n-24)*4;
+    }
+};
