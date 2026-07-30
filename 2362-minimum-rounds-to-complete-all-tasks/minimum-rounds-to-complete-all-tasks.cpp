7d1f89926ac1b77b53054b0913e185791fcf493c
KkrishnaGarg <24ucc029@lnmiit.ac.in>
2026-07-31 04:21:51 +0530

    Time: 31 ms (52.71%) | Memory: 107.5 MB (52.52%) - LeetSync

--- 2362-minimum-rounds-to-complete-all-tasks/minimum-rounds-to-complete-all-tasks.cpp
+++ 2362-minimum-rounds-to-complete-all-tasks/minimum-rounds-to-complete-all-tasks.cpp
@@ -8,8 +8,7 @@ public:
         int ans = 0;
         for(auto m : mpp){
             if(m.second == 1) return -1;
-            else if(m.second%3==0) ans += m.second/3;
-            else ans += m.second/3 + 1;
+            else ans += (m.second + 2)/3;
         }
         return ans;
     }
