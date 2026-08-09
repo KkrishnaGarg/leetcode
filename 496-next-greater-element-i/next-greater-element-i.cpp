28455429960572d387c15c60cd34b2e804622568
KkrishnaGarg <24ucc029@lnmiit.ac.in>
2026-08-09 20:19:28 +0530

    Time: 3 ms (40.67%) | Memory: 12.7 MB (63.51%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -4,7 +4,6 @@ public:
         int n = nums2.size();
         stack<int> st;
         unordered_map<int, int>mpp;
-        vector<int> nge(n,-1);
         for(int i=n-1; i>=0; i--){
             while( !st.empty() && st.top()<= nums2[i]) st.pop();
             if(st.empty()) mpp[nums2[i]] = -1;
@@ -12,9 +11,8 @@ public:
             st.push(nums2[i]);
         }
         vector<int> ans;
-        int m = nums1.size();
-        for(int i=0; i<m; i++){
-            ans.push_back(mpp[nums1[i]]);
+        for(auto x : nums1){
+            ans.push_back(mpp[x]);
         }
         return ans;
     }
