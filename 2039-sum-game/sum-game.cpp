4fb087a681fbad8008febc0cffa0d8adfcd9eb17
KkrishnaGarg <24ucc029@lnmiit.ac.in>
2026-08-23 20:13:48 +0530

    Time: 4 ms (47.55%) | Memory: 14.1 MB (10.26%) - LeetSync

--- 2039-sum-game/sum-game.cpp
+++ 2039-sum-game/sum-game.cpp
@@ -3,51 +3,51 @@ public:
     bool sumGame(string num) {
         int n = num.size();
 
-        int a=0, b=0;
-        for(int i=0; i<n; i++){
-            if(num[i] == '?'){
-                if(i<n/2) b++;
-                else a++;
+        int leftQ = 0, rightQ = 0;
+        int leftSum = 0, rightSum = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (num[i] == '?') {
+                if (i < n / 2)
+                    leftQ++;
+                else
+                    rightQ++;
+            }
+            else {
+                if (i < n / 2)
+                    leftSum += num[i] - '0';
+                else
+                    rightSum += num[i] - '0';
             }
         }
 
-        int before_half = 0, after_half = 0;
-        for(int i=0; i<n; i++){
-            if(i < n/2 && num[i]!='?') before_half += num[i] - '0';
-            else if(i >= n/2 && num[i]!='?') after_half += num[i] - '0';
-        }
+        // No '?' just check sum
+        if (leftQ + rightQ == 0)
+            return leftSum != rightSum;
 
-        // if no '?' then just check sum of before and after
-        if(a+b==0) return before_half != after_half;
+        // Odd number of '?' alice always wins
+        if ((leftQ + rightQ) % 2)
+            return true;
 
-        // if odd alice will always win
-        else if((a+b)%2==1) return true;
+        // Same number of '?' (whatever alice change bob will do so if sum is equal)
+        if (leftQ == rightQ)
+            return leftSum != rightSum;
 
-        // in case of even
-        else{
+        // Right side has more '?' and already has >= sum (alice will increase all by 9)
+        if (rightQ > leftQ && rightSum >= leftSum)
+            return true;
 
-            // agar left'?' == right'?' 
-            if(a==b){
-                // alice will increase the greater and bob won't be able to catch
-                if(before_half!=after_half) return true; // alice win
-                // bob will increase same on other side what alice did
-                else return false; // bob win
-            }
-            // agar left'?' != right'?' 
-            else{// one side will have more '?' in way 2,4,6,8,10..........
+        // Left side has more '?' and already has >= sum (alice will increase all by 9)
+        if (leftQ > rightQ && leftSum >= rightSum)
+            return true;
 
-                // side where we have more'?' and greater or equal sum both 
-                // alice will increase all by 9
-                if ((a>b && after_half >= before_half)||(a<b && after_half <= before_half)) return true; // alice win
+        // One side has more '?' but smaller sum.
+        // alice will do extreme either wont change i.e. 0 or will change fully i.e. 9
+        // bob will check if alice doing extreme 9, he will do 0 and vice-verse
+        int sumDiff = abs(leftSum - rightSum);
+        int qDiff = abs(leftQ - rightQ);
 
-                // side where we have more'?' and lesser sum
-                // alice will do extreme either wont change i.e. 0 or will change fully i.e. 9
-                // bob will check if alice doing extreme 9, he will do 0 and vice-verse
-                int abs_sum = abs(before_half - after_half);
-                int abs_count = abs(a-b);
-                if(abs_sum == abs_count/2 * 9) return false; // bob win
-            }
-        }
-        return true; // alice win
+        if(sumDiff == qDiff/2 * 9) return  false; // bob wins
+        else return true; // alice win
     }
 };
