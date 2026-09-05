dfe88360656aa77a61def461083cfcc1de68a7ee
KkrishnaGarg <24ucc029@lnmiit.ac.in>
2026-09-05 05:38:06 +0530

    Time: 10 ms (60.48%) | Memory: 202.7 MB (76.32%) - LeetSync

--- 4285-smallest-stable-index-ii/smallest-stable-index-ii.cpp
+++ 4285-smallest-stable-index-ii/smallest-stable-index-ii.cpp
@@ -2,17 +2,15 @@ class Solution {
 public:
     int firstStableIndex(vector<int>& nums, int k) {
         int n = nums.size();
-        vector<int> leftMax(n);
         vector<int> rightMin(n);
         int large = INT_MIN, small = INT_MAX;
         for(int i=0; i<n; i++){
-            large = max(large, nums[i]);
-            leftMax[i] = large;
             small = min(small, nums[n-1-i]);
             rightMin[n-1-i] = small;
         }
         for(int i=0; i<n; i++){
-            int diff = leftMax[i]-rightMin[i];
+            large = max(large, nums[i]);
+            int diff = large-rightMin[i];
             if(diff<=k) return i;
         }
         return -1;
