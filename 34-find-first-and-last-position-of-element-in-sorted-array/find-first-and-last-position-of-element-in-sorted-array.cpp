43a54b27349f225607d5c318df8aa538246e55a5
KkrishnaGarg <24ucc029@lnmiit.ac.in>
2026-09-12 00:31:04 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (88.62%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,28 +1,9 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        bool bs = binary_search(nums.begin(), nums.end(), target);
-        if(!bs) return{-1, -1};
-        int first = -1, last = nums.size();
-        int low=0, high = nums.size()-1;
-        while(high >= low){
-            int mid = (low+high)/2;
-            if(nums[mid]>=target){
-                first = mid;
-                high = mid-1;
-            }
-            else low = mid+1;
-        }
-
-        low=0, high = nums.size()-1;
-        while(high >= low){
-            int mid = (low+high)/2;
-            if(nums[mid]>=target+1){
-                last = mid;
-                high = mid-1;
-            }
-            else low = mid+1;
-        }
-        return {first, last-1};
+        int low = lower_bound(nums.begin(), nums.end(), target) - nums.begin();
+        if(low == nums.size() || nums[low]!=target) return {-1,-1};
+        int high = upper_bound(nums.begin(), nums.end(), target) - nums.begin();
+        return {low, high-1};
     }
 };
